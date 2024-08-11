3864fd720838432a6c588c52b44e7d7f2c4b5522
Dharmesh53 <dhiru7321r@gmail.com>
2024-08-11 23:25:56 +0530

    nvim tweaks

--- config/oneday/lua/ninjafire/lazy.lua
+++ config/oneday/lua/ninjafire/lazy.lua
@@ -1,37 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", 
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    { import = "ninjafire.plugins" },
---  { import = "ninjafire.plugins.lsp" }
-  },
-  
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "gruvbox" } },
+	spec = {
+		{ import = "ninjafire.plugins" },
+		--  { import = "ninjafire.plugins.lsp" }
+	},
 
-  change_detection = {
-    notify = false,
-  },
-  -- automatically check for plugin updates
-  checker = { enabled = true, notify = false,},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "gruvbox-material" } },
+
+	change_detection = {
+		notify = false,
+	},
+	-- automatically check for plugin updates
+	checker = { enabled = true, notify = false },
 })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
